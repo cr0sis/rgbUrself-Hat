c5fa0a1ffec85ea6c6ae1975da883c2da2978bfb
cr0sis <64103294+cr0sis@users.noreply.github.com>
2020-07-06 18:16:03 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+HOST = "irc.twitch.tv"                        # This is Twitchs IRC server
+PORT = 6667                                   # Twitch IRC server listens on port 6767
+NICK = "Yourbot"                                    # Twitch username your using for your bot
+PASS = "oauth:123456fnsdlfnwslfnslfnslfnslfn" # your Twitch OAuth token
+CHAN = "#yourchan"                              # The channel you want the bot to join. :D
+RATE = (20/30)                                # messages per seccond
+BAN_PAT = [
+    r"BAD_VVORD_HERE",
+    r"ANOTHER_NAUGHTY_VVORD_HERE" 
+]
+COMMANDS = [
+    [r"!discord", "The official discord is: coming soon!"],
+    [r"following", "Running lightshow routine."],
+    [r"subscribed", "Running lightshow routine."],
+    [r"!default", "Flooping lights to full bright."],
+    [r"!day", "Flooping lights to day."],
+    [r"!warm", "Flooping lights to warm."],
+    [r"!nm", "Flooping lights to night mode."],
+    [r"tipped", "Running lightshow routine."],
+    [r"cheered", "Running lightshow."],
+    [r"raided", "Running lightshow."],
+    [r"gifted", "Running lightshow ."],
+    [r"hosted", "Running lightshow."],
+    [r"flooped the hat to:", "Hat colour will be changed for 69 passes ASAP, if it was changed recently you can queue up the next colour before it goes back to rainbow"]
+
+]
